8d84feee751a8c023c73b125ad307dea80ecf83c
trananhduy <tanhduy160620@gmail.com>
2022-01-25 08:58:30 +0700

    fix bug font size in master screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_stateful_widget/screens/landing_screen.dart';
 import 'package:flutter_stateful_widget/utils/contants.dart';
 import 'dart:ui';
 void main() {
-  
  runApp(MyApp());
 }
 
@@ -18,11 +16,9 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Design UI 1',
+      title: 'Flutter Demo UI 1',
       theme: ThemeData(
-        
         primaryColor: COLOR_WHITE, 
-        accentColor: COLOR_DARK_BLUE, 
         textTheme: screenWidth < 500 ? TEXT_THEME_SMALL : TEXT_THEME_DEFAULT, 
         fontFamily: "Montserrat"),
       home: LandingScreen(),
--- lib/utils/custom_functions.dart
+++ lib/utils/custom_functions.dart
@@ -1,5 +1,6 @@
 import 'package:intl/intl.dart';
 
+// format tiền tệ, example 200000 => $200,000
 String formatConcurrency(num amount, {int decimalCount = 0}) {
   final formatConcurrency = NumberFormat.simpleCurrency(decimalDigits: decimalCount);
   return formatConcurrency.format(amount);
